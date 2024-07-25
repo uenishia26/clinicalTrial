3c340a99313544bc6d76707c54f3bbff07ad20a2
Anamu Uenishi <123331064+uenishia26@users.noreply.github.com>
2024-07-25 16:53:54 -0400

    With predictions and testing
    90% F1 Macro Score

--- firstKerasNeuralNetwork.py
+++ firstKerasNeuralNetwork.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense
+from sklearn.metrics import classification_report
 
 """
 Data: 
@@ -17,7 +18,10 @@ A clinical trial was done for a new drug between the ages of 13 to 100
 """
 trainSamples = [] #Indicatesd the age 
 trainLabels = [] #1: Did expericne side effects, 0: did not experience side effects 
+testSamples = []
+testLabels = []
 
+#Train dataset 
 #Data for the 5% 
 for i in range(50): 
     #5% of Pateints under 65 that did experince side effects 
@@ -43,13 +47,43 @@ for i in range(1000):
     trainSamples.append(random_younger)
     trainLabels.append(1)
 
+#Test Set 
+for i in range(25): 
+    #5% of Pateints under 65 that did experince side effects 
+    random_younger = randint(13,64)
+    testSamples.append(random_younger)
+    testLabels.append(1)
+
+    #5% of 65 and older that didn't experince side effects
+    random_older = randint(65, 100)
+    testSamples.append(random_older)
+    testLabels.append(0)
+
+#Data for the 95%
+for i in range(250): 
+
+    #95% of 65 and younger that didn't experince side effects 
+    random_younger = randint(13,64)
+    testSamples.append(random_younger)
+    testLabels.append(0)
+
+    #95% of 65 and older that did experince side effects
+    random_younger = randint(65,100)
+    testSamples.append(random_younger)
+    testLabels.append(1)
+
+
 train_labels = np.array(trainLabels)
 train_samples = np.array(trainSamples)
+test_labels = np.array(testLabels)
+test_samples = np.array(testSamples)
 train_labels, train_samples = shuffle(train_labels, train_samples)
+test_labels, test_samples = shuffle(test_labels, test_samples)
 
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_train_sample = scaler.fit_transform(train_samples.reshape(-1,1))
-print(scaled_train_sample)
+scaled_test_sample = scaler.fit_transform(test_samples.reshape(-1,1))
+
 
 
 model = Sequential([
@@ -61,6 +95,12 @@ model = Sequential([
 model.compile(optimizer=Adam(learning_rate=0.0001),loss='sparse_categorical_crossentropy',metrics=['accuracy'])
 
 #trainning
-model.fit(x=scaled_train_sample, y=train_labels, batch_size=10, epochs=30, shuffle=True, verbose=2)
+#Note: validation_set is take from the last 10% of the data thus you must shuffle data before calling the fit function 
+model.fit(x=scaled_train_sample, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=2)
 
+predictions = model.predict(x=scaled_test_sample, batch_size=10)
 
+rounded_predictions = np.argmax(predictions, axis=-1)
+
+classification_report(rounded_predictions, test_labels)
+print(classification_report(rounded_predictions, test_labels))
